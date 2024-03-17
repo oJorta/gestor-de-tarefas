2dcefb2eec5008afe1493fb5f7944ddbdd957f44
Joao Pedro <jpcsnoberto@gmail.com>
2024-03-17 16:46:30 -0300

    fix: finished task styling and responsive css

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import styles from './task.module.css'
 
 import { FiTrash } from "react-icons/fi";
 import Priority from '../Priority';
+import { useEffect, useState } from 'react';
 
 interface TaskProps {
     title: string;
@@ -12,26 +15,36 @@ interface TaskProps {
 }
 
 export default function Task({ title, description, priority, deadline, onDelete }: TaskProps) {
-    return (
-        <>
-            <div className={styles.taskContainer}>
-                <header>
-                    <h3>{title}</h3>
-                    <div className={styles.options}>
-                        <input type="checkbox" name="" id="" />
-                        <button onClick={onDelete}>
-                            <FiTrash/>
-                        </button>
-                    </div>
-                </header>
+    const [isChecked, setIsChecked] = useState(priority === 'finished' ? true : false)
 
-                <div className={styles.taskInfo}>
-                    {priority && <Priority type={priority} />}
-                    {deadline && <p>Prazo: {deadline.toLocaleDateString()}</p>}
-                </div>
-                
-                {description && <p className={styles.description}>{description}</p>}
+    return (
+      <>
+        <div className={`${styles.taskContainer} ${isChecked && styles.finished}`}>
+          <header>
+            <h3>{title}</h3>
+            <div className={styles.options}>
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={() => setIsChecked(!isChecked)}
+              />
+              <button onClick={onDelete}>
+                <FiTrash />
+              </button>
             </div>
-        </>
-    )
+          </header>
+
+          <div className={styles.taskInfo}>
+            {priority && !isChecked ? (
+              <Priority type={priority} />
+            ) : (
+              isChecked && <Priority type="finished" />
+            )}
+            {deadline && <p>Prazo: {deadline.toLocaleDateString()}</p>}
+          </div>
+
+          {description && <p className={styles.description}>{description}</p>}
+        </div>
+      </>
+    );
 }
